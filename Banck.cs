d70eaba388f846fd6a432cb710e1e7af8dbec52d
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2026-02-12 19:41:26 -0300

    Agregar archivos de proyecto.

---
+++ Banck.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banck_Operator
+{
+    public class Banck
+    {
+        private static int s_accountNumberSeed = 1234567890;
+        public string Owner { get; set; }
+        public string? Number { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in _allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException(nameof(amount), "El monto debe ser mayor a cero");
+            }
+            var deposit = new Transaction(amount, date, note);
+            _allTransactions.Add(deposit);
+        }
+
+        public void Makewhithdrawall(decimal amount, DateTime date, string note)
+        {
+
+            if (amount <= 0)
+            {
+                throw new ArgumentException(nameof(amount), "El monto debe ser mayor a cero");
+            }
+            if ((Balance - amount) < 0)
+            {
+                throw new InvalidOperationException("No tienes suficiente fondos en withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            _allTransactions.Add(withdrawal);
+        }
+
+        public Banck(string owner, decimal introBalance)
+        {
+            Owner = owner;
+            MakeDeposit(introBalance, DateTime.Now, "Initials Counts");
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+        }
+
+        private List<Transaction> _allTransactions = new List<Transaction>();
+    }
+    public record Transaction(decimal Amount, DateTime Date, string Notes);
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Banck_Operator;
+
+var banck = new Banck("<Name>", 10000);
+
+banck.Makewhithdrawall(5500, DateTime.Now, "Extraccion de 5500");
+Console.WriteLine($"Su cuenta tiene: {banck.Number}: {banck.Balance}");
+banck.MakeDeposit(50, DateTime.Now, "Deposito de 50");
+Console.WriteLine($"Su cuenta tiene: {banck.Number}: {banck.Balance}");
+Console.ReadLine();
