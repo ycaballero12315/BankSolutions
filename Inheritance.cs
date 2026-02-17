c02ca5f949324ca2856e45dcc206a246e0c47700
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2026-02-17 09:31:35 -0300

    ejemplo de herencia

---
+++ Inheritance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSolutions
+{
+    public abstract class A
+    {
+        private int _number = 34;
+
+
+        public abstract void Element();
+        public abstract class B : A
+        {
+            public int PrintNumber()
+            {
+                return _number;
+            }
+
+            public override void Element()
+            {
+                Console.WriteLine("Yo si heredo, soy abstracto.");
+            }
+        }
+    }
+    public class C : A
+    {
+        public override void Element()
+        {
+            Console.WriteLine("Yo si heredo, soy abstracto. Pero desde C");
+        }
+    }
+
+    public record Person(string Name, int Age);
+}
